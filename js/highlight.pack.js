38f72d136e7fbb8847089ce4f0e96781596d4626
joente <joente@users.noreply.github.com>
2022-05-31 14:26:11 +0000

    deploy: d23fef028149ea0ad333d9bc8616701cb74800f3

--- js/highlight.pack.js
+++ js/highlight.pack.js
@@ -35,7 +35,7 @@
   }
 
 
-  
+
   function inherit(parent) {  // inherit(parent, override_obj, override_obj, ...)
     var key;
     var result = {};
@@ -1773,7 +1773,7 @@ hljs.registerLanguage('siridb', function () {
     Language: SiriDB
     Author: Jeroen van der Heijden <jeroen@cesbit.com>
     Category: common
-    Website: https://siridb.net
+    Website: https://siridb.com
     */
 
     function siridb(hljs) {
